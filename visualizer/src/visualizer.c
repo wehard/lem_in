6044188da1f6a26b353fa1beb6017ab4548ca592
Willehard Korander <willehard@gmail.com>
2020-03-14 16:51:18 +0200

    mouse zoom. room colors

--- visualizer/src/visualizer.c
+++ visualizer/src/visualizer.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/14 10:29:19 by wkorande          #+#    #+#             */
-/*   Updated: 2020/03/14 13:55:20 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/03/14 16:50:30 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 #include "ft_printf.h"
 #include "ft_get_next_line.h"
 
-void draw_rooms(t_list *rooms, SDL_Renderer *renderer)
+void draw_rooms(t_list *rooms, double zoom, SDL_Renderer *renderer)
 {
 	t_list *cur;
-	int zoom;
 	int size;
 
 	size = 20;
-	zoom = 30;
 	cur = rooms;
 	while (cur)
 	{
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		t_room r = *(t_room*)cur->content;
+		if (r.is_start)
+			SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+		else if (r.is_end)
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		else
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		SDL_Rect rect;
 		rect.x = ((int)r.coord.x  * zoom) - (size / 2);
 		rect.y = ((int)r.coord.y  * zoom) - (size / 2);
@@ -38,12 +41,10 @@ void draw_rooms(t_list *rooms, SDL_Renderer *renderer)
 	}
 }
 
-void draw_links(t_list *links, SDL_Renderer *renderer)
+void draw_links(t_list *links, double zoom, SDL_Renderer *renderer)
 {
 	t_list *cur;
-	int zoom;
 
-	zoom = 30;
 	cur = links;
 	while (cur)
 	{
@@ -60,6 +61,7 @@ int main(void)
 	SDL_Renderer *renderer;
 
 	t_env *env;
+	double zoom;
 
 	env = init_env();
 	read_env(env);
@@ -72,6 +74,7 @@ int main(void)
 	win = SDL_CreateWindow("lem_in", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
 	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	int quit = 0;
+	zoom = 50;
 	while (!quit)
 	{
 		SDL_Event event;
@@ -85,9 +88,18 @@ int main(void)
 				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
 					quit = 1;
 			}
+			else if (event.type == SDL_MOUSEWHEEL)
+			{
+				if (event.wheel.y > 0)
+					zoom *= 1.1;
+				else if (event.wheel.y < 0)
+					zoom *= 0.9;
+			}
 		}
-		draw_links(env->links, renderer);
-		draw_rooms(env->rooms, renderer);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		draw_links(env->links, zoom, renderer);
+		draw_rooms(env->rooms, zoom, renderer);
 		SDL_RenderPresent(renderer);
 	}
 	SDL_DestroyWindow(win);
