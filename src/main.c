2cb8192f20e9e238c41447113f1ab4aa75c9cca0
Willehard Korander <willehard@gmail.com>
2020-03-18 11:56:57 +0200

    debug printout

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 12:38:35 by wkorande          #+#    #+#             */
-/*   Updated: 2020/03/18 01:27:37 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/03/18 11:38:08 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ int move_ant(t_ant *ant, t_room *room)
 {
 	if (ant->cur_room->type == END)
 	{
-		debug_log("move_ant: ant is in end room\n");
+		debug_log("move_ant: ant %d is in end room (name: %s)\n", ant->id, ant->cur_room->name);
 		return (0);
 	}
 	if (room->type != END && room->occupied)
 	{
-		debug_log("move_ant: room occupied!\n");
+		debug_log("move_ant: room occupied! ant %d tried room %s\n", ant->id, room->name);
 		return (0);
 	}
 	ant->cur_room->occupied = 0;
